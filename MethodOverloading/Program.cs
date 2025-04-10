e0318e7bb873fcf7e1df1abdb641f5f03b710865
Daniel Page <dpage105@gmail.com>
2025-04-10 18:12:51 -0500

    Completed Method Overloading Exercise 1

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace MethodOverloading
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MethodOverloading
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(8, -6));
+            Console.WriteLine(Add(8.7m, -9.4m));
+            Console.WriteLine(Add(1, 0, true));
+            Console.WriteLine(Add(5, 7, true));
+            Console.WriteLine(Add(8, 23, false));
+        }
+
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+        
+        public static string Add(int num1, int num2, bool dollars)
+        {
+            var sum = num1 + num2;
+
+            if (dollars)
+            {
+                return sum == 1 ? $"{sum} dollar" : $"{sum} dollars";
+            }
+            else return $"{sum}";
         }
     }
 }
